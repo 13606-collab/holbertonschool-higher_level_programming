b808f8fdc5e405c3b313adc8580d423738cf6a1f
13606-collab <13606@holertonstudents.com>
2026-03-21 17:35:22 +0000

    estegg

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Beautiful is better than ugly.")
+print("Explicit is better than implicit.")
+print("Simple is better than complex.")
+print("Complex is better than complicated.")
+print("Flat is better than nested.")
+print("Sparse is better than dense.")
+print("Readability counts.")
