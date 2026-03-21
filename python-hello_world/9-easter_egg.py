28e689af6d8f3be806b0a30413cf540e947e3e32
13606-collab <13606@holertonstudents.com>
2026-03-21 17:54:01 +0000

    estegg

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-print("Beautiful is better than ugly.")
-print("Explicit is better than implicit.")
-print("Simple is better than complex.")
-print("Complex is better than complicated.")
-print("Flat is better than nested.")
-print("Sparse is better than dense.")
-print("Readability counts.")
+str = "The Zen of Python, by Tim Peters"
+print(str)
