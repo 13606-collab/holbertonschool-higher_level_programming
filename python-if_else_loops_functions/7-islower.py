e9536fa50d235348bab4c0cbfcef310f3ab2e282
13606-collab <13606@holbertonstudents.com>
2026-03-22 08:15:15 +0000

    if7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     if ord(c) in range(91, 116):
-        print("{} is upper".format(c))
-    else:
         print("{} is lower".format(c))
+    else:
+        print("{} is upper".format(c))
 islower(c="A")
