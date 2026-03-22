688be421741bba420cb2fb2615ad6ed47e1a0904
13606-collab <13606-collab@holbertonstudensts.com>
2026-03-22 17:52:11 +0000

    if100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-str=""
-for i in range(90,64,-1):
+str = ""
+for i in range(90 ,64 ,-1):
     if i % 2 == 0:
-        str+=chr(i+32)
+        str += chr(i+32)
     else:
-        str+=chr(i)
-print("{}".format(str),end="")
+        str += chr(i)
+print("{}".format(str), end = "")
