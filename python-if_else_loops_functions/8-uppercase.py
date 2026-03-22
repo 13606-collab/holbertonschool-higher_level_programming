96ee50cc12540161b01a1156681178f79236e446
13606-collab <13606-collab@holbertonstudensts.com>
2026-03-22 12:01:16 +0000

    if8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def uppercase(c):
     strc = ''
-    for i in str(c):
+    for i in c:
         if ord(i) in range (65, 91):
             strc += str(i)
         elif ord(i) in range (97, 123):
-            strc += str(chr(ord(i)-32))
+            strc += chr(ord(i)-32)
         else:
             strc += i
     print("{}".format(strc))
