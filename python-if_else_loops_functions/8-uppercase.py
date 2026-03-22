a3cb7106e55b04293d20ec154309e832c63185a8
13606-collab <13606-collab@holbertonstudensts.com>
2026-03-22 12:03:16 +0000

    if8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(c):
     strc = ''
     for i in c:
-        if ord(i) in range (65, 91):
+        if ord(i) in range(65, 91):
             strc += str(i)
-        elif ord(i) in range (97, 123):
+        elif ord(i) in range(97, 123):
             strc += chr(ord(i)-32)
         else:
             strc += i
