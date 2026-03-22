15e730490977b080601933ad490a84efcfbb2981
13606-collab <13606-collab@holbertonstudensts.com>
2026-03-22 12:47:01 +0000

    if9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,5 @@
 def print_last_digit(number):
     if number%10 == 0:
         print(0)
-    elif number >= 0:
-        print(number%10)
     else:
-        print(10-number%10)
+        print(abs(number)%10)
