07b4805972f489f0b48bf97b1cc3b7c879bdcd3b
13606-collab <13606@holbertonstudents.com>
2026-03-24 08:06:15 +0000

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-from calculator import add
-from calculator import sub
-from calculator import mul
-from calculator import div
+from calculator_1 import *
 
 if __name__ == "__main__":
     a = 10
