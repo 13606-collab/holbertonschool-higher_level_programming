1080005e2f3e0717fa780f0de8f8cd7350abde81
13606-collab <13606@holbertonstudents.com>
2026-03-24 13:38:39 +0000

    imp3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    
-    s = 0
- 
+        s = 0
     for i in sys.argv[1:]:
         s += int(i)
-    print("{}".format(s), end = "\n")
+    print("{}".format(s), end="\n")
