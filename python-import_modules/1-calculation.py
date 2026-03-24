0ca4867a7d5f7bb257561ae2d28fe2252a34a580
13606-collab <13606@holbertonstudents.com>
2026-03-24 08:04:39 +0000

    calculation

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator import add
+from calculator import sub
+from calculator import mul
+from calculator import div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    
+    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} - {} = {}".format(a, b, add(a,b)))
+    print("{} * {} = {}".format(a, b, add(a,b)))
+    print("{} / {} = {}".format(a, b, add(a,b)))
