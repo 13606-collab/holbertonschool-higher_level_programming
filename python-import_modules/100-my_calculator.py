8eed34eaaa01a454638ae6b71f232e8a678bf174
13606-collab <13606@holbertonstudents.com>
2026-03-24 17:06:53 +0000

    imp100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     a = int(sys.argv[1])
     o = sys.argv[2]
@@ -20,4 +21,4 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, o, b, div(a, b))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
