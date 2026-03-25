6f2e4caf8cddf6463fc403e841d6b02b146ebdd1
13606-collab <13606@holbertonstudents>
2026-03-25 11:43:30 +0000

    data11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def delete_at(my_list=[1,2,3,4,5], idx=9):
+def delete_at(my_list=[], idx=0):
     new_list = []
     if idx < 0 or idx >= len(my_list):
         new_list[:] = my_list
