dadd30290305b95496cc507ba55f48153e1f04cb
13606-collab <13606@holbertonstudents.com>
2026-03-25 07:55:21 +0000

    data7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(1, 89), tuple_b=(88, 11)):
+    if len(tuple_a)>0:
+        a1=tuple_a[0]
+    if len(tuple_b)>0:
+        b1=tuple_b[0]
+    if len(tuple_a)>1:
+        a2=tuple_a[1]
+    if len(tuple_b)>1:
+        b2=tuple_b[1]
+    return a1+b1, a2+b2
