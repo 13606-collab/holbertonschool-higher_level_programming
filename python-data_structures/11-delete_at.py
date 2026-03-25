7798fbb5b7592cd43332ba9db5059a484b078eee
13606-collab <13606@holbertonstudents>
2026-03-25 11:50:10 +0000

    data11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,9 +2,9 @@
 def delete_at(my_list = [], idx = 0):
     new_list = []
     if idx < 0 or idx >= len(my_list):
-        new_list[:] = my_list
-        return new_list
+        return my_list
     for i in range(len(my_list)):
         if not i == idx:
             new_list.append(my_list[i])
+    my_list[:] = new_list
     return new_list
