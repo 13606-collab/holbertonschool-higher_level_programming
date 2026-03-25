cd02a85370f06bb8fea9f0359a74611ca7456eba
13606-collab <13606@holbertonstudents.com>
2026-03-25 08:13:53 +0000

    data8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
-    length = len(sentence)
-    first = sentence[0]
-    return("Length: {:d} - First character: {}".format(length, first))
+        length = 0
+        first = None
+    else:
+        length = len(sentence)
+        first = sentence[0]
+    return length, first
