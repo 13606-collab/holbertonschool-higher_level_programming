bedb6343290739ca6bbf1d003eca2d65d93975a1
13606-collab <13606@holbertonstudents.com>
2026-03-26 07:40:30 +0000

    more0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda j:list(map(lambda x:x**2, j)),matrix))
+    new_matrix = list(map(lambda j: list(map(lambda x: x**2, j)), matrix))
     return new_matrix
