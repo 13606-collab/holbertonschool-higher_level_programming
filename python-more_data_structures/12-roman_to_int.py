5342455b206a85f9d8c63cf3bace8504a1fa2efd
13606-collab <13606@holbertonstudents.com>
2026-03-27 10:05:50 +0000

    more_12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict = {"I":1, "V":5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    integer = 0
+    before = 1000
+    for i in roman_string:
+        if i not in roman_dict:
+            return 0
+        if roman_dict.get(i) > before:
+            integer = (integer + roman_dict.get(i) - 2*before)
+        else:
+            integer += roman_dict.get(i)
+        before = roman_dict.get(i)
+    return integer
