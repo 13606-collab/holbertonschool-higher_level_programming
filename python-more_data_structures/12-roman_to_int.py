ecac76f32a0286622299fb29542592247f32ab53
13606-collab <13606@holbertonstudents.com>
2026-03-27 10:45:24 +0000

    more_12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
     roman_dict = {"I":1, "V":5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     integer = 0
     before = 1000
@@ -7,7 +9,7 @@ def roman_to_int(roman_string):
         if i not in roman_dict:
             return 0
         if roman_dict.get(i) > before:
-            integer = (integer + roman_dict.get(i) - 2*before)
+            integer = (integer + roman_dict.get(i) - 2 * before)
         else:
             integer += roman_dict.get(i)
         before = roman_dict.get(i)
