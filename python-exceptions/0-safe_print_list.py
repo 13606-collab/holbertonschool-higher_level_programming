095ff9a4f1c5847910f480ee530961237eaf2342
13606-collab <13606@holbertonstudents.com>
2026-03-30 10:07:19 +0000

    exc0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        return my_list[:x]
+        for i in range(x):
+            print("{}".format(my_list[i]), end = "")
     except:
-        return my_list[-1]   
+        return x  
