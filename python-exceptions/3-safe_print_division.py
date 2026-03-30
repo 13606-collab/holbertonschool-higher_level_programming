28dd67aa0250b452024367b1e53e10dcd811a3e9
13606-collab <13606@holbertonstudents.com>
2026-03-30 13:45:44 +0000

    exc3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,4 +5,4 @@ def safe_print_division(a, b):
     except:
         result =  None
     finally:
-        print("section preceded by Inside result:{:d} / {:d} = {}".format(a, b, result))
+        print("section preceded by Inside result:{}".format(result))
