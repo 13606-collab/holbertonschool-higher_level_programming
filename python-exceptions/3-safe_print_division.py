402a55f65061b50e286b20184d9c0e583e351d31
13606-collab <13606@holbertonstudents.com>
2026-03-30 14:02:55 +0000

    exc3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,9 @@
 def safe_print_division(a, b):
     try:
         result = a/b
+        return result
     except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
+        return result
