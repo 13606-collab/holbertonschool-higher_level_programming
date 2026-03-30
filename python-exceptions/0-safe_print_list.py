9f9b143ca2af9e54cffa97c62cdf002305759d30
13606-collab <13606@holbertonstudents.com>
2026-03-30 10:25:43 +0000

    exc0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,4 +6,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end = "")
             say += 1
         except:
-            return say  
+            break
+    return say
