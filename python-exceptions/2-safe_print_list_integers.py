45514e5a88d1f6d461a8fe4f30eb3c528f6f1259
13606-collab <13606@holbertonstudents.com>
2026-03-31 08:24:07 +0000

    exc2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
 
     say = 0
-
     for i in range(x):
-
         try:
             print("{:d}".format(my_list[i]), end="")
             say += 1
@@ -14,4 +11,4 @@ def safe_print_list_integers(my_list=[], x=0):
         except IndexError:
             break
     print()
-    print(say)
+    return say
