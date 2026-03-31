5d6c741d256221fc37ed15b216b893e5c608e480
13606-collab <13606@holbertonstudents.com>
2026-03-31 07:46:15 +0000

    exc100

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value), end="")
+        print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as error:
         print("Exception: {}".format(error))
