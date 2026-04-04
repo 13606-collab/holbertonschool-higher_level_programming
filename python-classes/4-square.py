a35d380ab3d2517b2a5aaa08d145a55bb829c081
13606-collab <13606@holbertonstudents.com>
2026-04-04 17:28:31 +0000

    cls_4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,7 +1,6 @@
 class Square:
 
     def __init__(self, size=0):
-        
         self.size = size
 
     @property
@@ -12,9 +11,9 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
+        if value<0:
+            raise ValueError("size must be>=0")
         self.__size = value
 
     def area(self):
-        return self.__size ** 2
+        return self.__size**2
