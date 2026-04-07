0ca003c1febd255addad36cdda378e173772bfa5
13606-collab <13606@holbertonstudents.com>
2026-04-07 07:27:04 +0000

    cls3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""Module that defines a Square"""
+
 
 class Square:
+    """Represents a square with size validation"""
+
     def __init__(self, size=0):
+        """Initialize the square with optional size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
+
     def area(self):
-        return self.__size**2
+        """Returns the current square area"""
+        return self.__size ** 2
