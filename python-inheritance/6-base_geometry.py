8b79bba12e51b8c0a222d5c7622a7ff8087c76a1
13606-collab <13606@holbertonstudents.com>
2026-04-11 04:51:38 +0000

    inh_6 base geo

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Class added
+    """
+
+    def area(self):
+        """
+        This method return a message
+        """
+        raise Exception(area() is not implemented)
