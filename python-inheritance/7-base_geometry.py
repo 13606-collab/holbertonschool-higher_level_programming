a636995a007781381f80d281b682282a2fd7405a
13606-collab <13606@holbertonstudents.com>
2026-04-11 12:32:59 +0000

    inh_7 was created

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
         self.name = name
         self.value = value
 
-        if not type(obj) == int:
+        if not type(value) == int:
             raise TypeError("{} must be an integer".format(self.name))
 
         if value <= 0:
