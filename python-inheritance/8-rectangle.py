1f96cfe5633d8f316b8af4e7f4836b9b4eb1b792
13606-collab <13606@holbertonstudents.com>
2026-04-11 13:36:09 +0000

    inh 8 file was created

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,29 +4,7 @@ This is empty class
 """
 
 
-class BaseGeometry:
-    """
-    Class added
-    """
-
-    def area(self):
-        """
-        This method return a message
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        This method return a message
-        """
-        self.name = name
-        self.value = value
-
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(self.name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(self.name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
