71ab0fce7139789338f254eaa6aceb25d866dbf9
13606-collab <13606@holbertonstudents.com>
2026-04-11 12:07:33 +0000

    inh_7 was created

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This is empty class
+"""
+
+
+class BaseGeometry:
+    """
+    Class added
+    """
+
+    def area(self):
+        """
+        This method return a message
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This method return a message
+        """
+        self.name = name
+        self.value = value
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(self.name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
