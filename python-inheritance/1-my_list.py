d69888853cc50872bc0fa86a1f1e91345d8ec5b0
13606-collab <13606@holbertonstudents.com>
2026-04-11 02:18:55 +0000

    inh_1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,14 +8,12 @@ This module provides a function to list an object's attributes and methods.
 class MyList(list):
     """
     This class inherit from list
-    
     """
     
     def print_sorted(self):
         """
         Prints the list elements in ascending order.
         The original list remains unchanged.
-        
         """
 
         print(sorted(self))
