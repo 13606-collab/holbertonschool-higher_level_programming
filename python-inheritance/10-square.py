5fd2e0043296d82c60ca3b50afe1c5ea07622524
13606-collab <13606@holbertonstudents.com>
2026-04-11 17:09:43 +0000

    inheritace 10 file

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is empty class
+"""
+
+
+Rectangle = __import__('7-base_geometry').Rectangle
+
+
+class Square(Rectangle):
+    """
+    This class defines Square of Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        a new Square instance.
+        """
+        
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+
+        self.__size = size
