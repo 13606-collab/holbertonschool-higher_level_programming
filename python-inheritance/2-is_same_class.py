0c9dab2168dfd9db23c74f3cccc855ed76531978
13606-collab <13606@holbertonstudents.com>
2026-04-11 04:19:03 +0000

    inheritance_2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module represents if obj is instance of class
+"""
+
+def is_same_class(obj, a_class):
+    """
+    This method return True or False
+    """
+    return isinstance(obj, a_class)
