a44e12d567b9c578dfdc3c4d117084fafe708e50
13606-collab <13606@holbertonstudents.com>
2026-04-11 04:41:23 +0000

    inheritance 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,4 +8,4 @@ def inherits_from(obj, a_class):
     """
     This method return True or False
     """
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
