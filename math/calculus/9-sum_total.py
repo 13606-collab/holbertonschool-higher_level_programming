75dbc384905b7c6abbb41f7c18b8d11e3503611a
13606-collab <13606@holbertonstudents.com>
2026-04-13 23:37:30 +0000

    calculus 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -10,6 +10,6 @@ def summation_i_squared(n):
     if not isinstance(n, int) or n < 0:
         return None
 
-    sum = (n*(n+1)*(2*n+1))//6
+    s = (n*(n+1)*(2*n+1))//6
 
-    return sum
+    return s
