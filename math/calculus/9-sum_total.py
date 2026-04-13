4e6fd8aba099bccdad5228359f42ee83a2dd3789
13606-collab <13606@holbertonstudents.com>
2026-04-13 23:22:09 +0000

    calculus 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,14 +6,10 @@ sum calculation
 def summation_i_squared(n):
     """
     function for sum calculation
-    """
-
-    sum = 0
-    
+    """    
     if not n:
         return None
 
-    for i in range(1, n+1):
-        sum += i*i
+    sum = (n*(n+1)*(2*n+1))/6
 
-    return sum
+    return int(sum)
