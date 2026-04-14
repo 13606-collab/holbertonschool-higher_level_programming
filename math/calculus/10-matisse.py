259d1341670801deab153be60f2c2f0c01899509
13606-collab <13606@holbertonstudents.com>
2026-04-14 00:26:53 +0000

    calculus 10

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -8,9 +8,11 @@ def poly_derivative(poly):
     """
     function for poly
     """
-    if len(poly) < 2:
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
     new_poly = []
     for i in range(1, len(poly)):
         new_poly.append(i*poly[i])
+    if new_poly == []:
+        return [0]
     return new_poly
