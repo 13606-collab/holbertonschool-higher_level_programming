7f1cae08ab89a7c19473755389f8adaece059659
13606-collab <13606@holbertonstudents.com>
2026-04-14 01:11:56 +0000

    calculus 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -11,7 +11,7 @@ def poly_integral(poly, C=0):
     if not isinstance(poly, list) or not isinstance(C, int):
         return None
     new_poly = [C]
-    for i in range(1, len(poly)):
+    for i in range(len(poly)):
         x = poly[i]/(i+1)
         if x.is_integer():
             new_poly.append(int(x))
