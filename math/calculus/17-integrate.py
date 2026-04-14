d1f5bccfa9d85808aa6a4668e10a257da7a42eee
13606-collab <13606@holbertonstudents.com>
2026-04-14 00:51:07 +0000

    calculus 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,13 +4,13 @@ integral of poly
 """
 
 
-def poly_derivative(poly):
+def poly_integral(poly, C=0):
     """
     function for integral poly
     """
     if not isinstance(poly, list) or len(poly) == 0:
         return
-    new_poly = [0]
+    new_poly = [C]
     for i in range(1, len(poly)):
         new_poly.append(poly[i]/(i+1))
     if new_poly == []:
