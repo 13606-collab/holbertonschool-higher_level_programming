cccf957d286b3a7baafad2562ec0584d4f3fddcf
13606-collab <13606@holbertonstudents.com>
2026-04-20 06:46:46 +0000

    lin_alg_0

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" This code is for array slicing"""
-
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
 arr1 = arr[:2]
 arr2 = arr[-5:]
 arr3 = arr[1:6]
-print(arr1, arr2, arr3)
+print(arr1)
+print(arr2)
+print(arr3)
