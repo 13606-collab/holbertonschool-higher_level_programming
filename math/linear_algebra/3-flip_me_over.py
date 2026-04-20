3fe30a298d6b2d679fb030a98f4be75fb2e4b993
13606-collab <13606@holbertonstudents.com>
2026-04-20 08:10:20 +0000

    lin_alg_3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module defines transpose of matrix"""
+
+
+import numpy as np
+def matrix_transpose(matrix):
+    """Matrix transpose"""
+    transpose = np.transpose(matrix)
+    return transpose
