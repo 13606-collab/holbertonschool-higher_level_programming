61abeb3f4d587300c9cd61de438cd8afe98e64f5
13606-collab <13606@holbertonstudents.com>
2026-04-20 10:47:39 +0000

    lin_alg_5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""This module defines adding of 2D matrises"""
+
+
+def add_matrices2D(mat1, mat2):
+    """2D Matrix add"""
+    if not len(mat1) == len(mat2) or not len(mat1[0]) == len(mat2[0]):
+        return None
+    row = len(mat1)
+    column = len(mat1[0])
+    add_mat = [[0] * row for _ in range(column)]
+    for i in range(row):
+        for j in range(column):
+            add_mat[i][j] = mat1[i][j] + mat2[i][j]
+    return add_mat
