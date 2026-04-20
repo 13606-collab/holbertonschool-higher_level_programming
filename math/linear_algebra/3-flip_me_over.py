2d3e6bff38ecc2c0099df5ab5c65ed03b123580b
13606-collab <13606@holbertonstudents.com>
2026-04-20 10:22:16 +0000

    lin_alg_3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -4,12 +4,10 @@
 
 def matrix_transpose(matrix):
     """Matrix transpose"""
-    
     row = len(matrix)
     column = len(matrix[0])
 
     transpose = [[0] * row for _ in range(column)]
-    
     for i in range(row):
         for j in range(column):
             transpose[j][i] = matrix[i][j]
