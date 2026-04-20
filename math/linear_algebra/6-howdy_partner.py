6a7b341796a6c2bc6699d3bcac2a551157301093
13606-collab <13606@holbertonstudents.com>
2026-04-20 11:04:07 +0000

    lin_alg_6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines Concatenates of 2 matrises"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates of 2 matrises"""
+    cat_arr = arr1 + arr2
+    return cat_arr
