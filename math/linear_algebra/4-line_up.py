d7f541029e8f480048bb81a60fed80ce541e80a3
13606-collab <13606@holbertonstudents.com>
2026-04-20 10:33:53 +0000

    lin_alg_4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -4,7 +4,7 @@
 
 def add_arrays(arr1, arr2):
     """Matrix add"""
-    add = [0] * len(arr1)
+    add = [0] * max(len(arr1), len(arr2))
     for i in range(len(arr1)):
         add[i] = arr1[i] + arr2[i]
     return add
