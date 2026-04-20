03c5483d66e31e1360d7d4a29bbc77d30e7deb59
13606-collab <13606@holbertonstudents.com>
2026-04-20 11:37:21 +0000

    lin_alg_7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This module defines Concatenates of 2 matrises"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Concatenates of 2 matrices"""
+    new_matrix = mat1 + mat2
+    return new_matrix
