362d2f8d5456a56cf9c8b0e4130a2f6e9b6af984
13606-collab <13606@holbertonstudents.com>
2026-04-20 11:56:52 +0000

    lin_alg_7

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,5 +4,33 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenates of 2 matrices"""
-    new_matrix = mat1 + mat2
-    return new_matrix
+    if axis == 0:
+        for row in mat1:
+            mat1_columns = len(row)
+        for row in mat2:
+            mat2_columns = len(row)
+        if mat1_columns != mat2_columns:
+            return None
+        cat_matrix = []
+        for x, row in enumerate(mat1):
+            cat_matrix.append([])
+            for i in row:
+                cat_matrix[x].append(i)
+        x += 1
+        for y, row in enumerate(mat2):
+            cat_matrix.append([])
+            for i in row:
+                cat_matrix[x + y].append(i)
+        return cat_matrix
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        cat_matrix = []
+        for z, row in enumerate(mat1):
+            cat_matrix.append([])
+            for i in mat1[z]:
+                cat_matrix[z].append(i)
+            for i in mat2[z]:
+                cat_matrix[z].append(i)
+        return cat_matrix
+    return None
