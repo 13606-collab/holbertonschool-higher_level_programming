57e760669d34c8ea50b2a3f0faa15c99beeefea8
13606-collab <shamil1999fcb@gmail.com>
2026-04-22 01:24:01 +0400

    vscode100

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+
+"""_summary_"""
+def np_slice(matrix, axes={}):
+    """_summary_
+
+    Args:
+        matrix (_type_): _description_
+        axes (dict, optional): _description_. Defaults to {}.
+
+    Returns:
+        _type_: _description_
+    """
+    dimensions = len(matrix.shape)
+    slices_matrix = dimensions * [slice(None)]
+    for axis, value in axes.items():
+        slices_matrix[axis] = slice(*value)
+    return matrix[tuple(slices_matrix)]
