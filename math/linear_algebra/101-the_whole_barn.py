d9cffb070cff9c919e6be08ad4975075e229583d
shamilnabiyev <13606@holbertonstudents.com>
2026-04-24 15:29:11 +0400

    Validate matrix dimensions before addition
    Add validation for matrix dimensions in add_matrices function.

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -5,6 +5,9 @@
 def add_matrices(mat1, mat2):
     """Function for The Whole Barn"""
     # Initialize result matrix with zeros
+    
     if isinstance(mat1, list):
+        if not len(mat1) == len(mat2):
+            return None
         return [add_matrices(i, j) for i, j in zip(mat1, mat2)]
     return mat1+mat2
