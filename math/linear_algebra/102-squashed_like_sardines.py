04bc67d5aabb44dd9fb9a713ab01d55899ff3ace
shamilnabiyev <13606@holbertonstudents.com>
2026-04-25 19:11:50 +0400

    Add matrix_shape and update cat_matrices for recursion
    Added matrix_shape function to determine the shape of a matrix. Implemented recursive concatenation of two matrices with cat_matrices and rec functions.

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
-"""Squashed Like Sardines"""
+"""squashed like sardine"""
 
 
-import numpy as np
-import time
+def matrix_shape(matrix):
+    """ function for squashed like sardine"""
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    
+    """returns concatenation of two matrices"""
+    from copy import deepcopy
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if len(shape1) != len(shape2):
+        return None
+    for i in range(len(shape1)):
+        if i != axis:
+            if shape1[i] != shape2[i]:
+                return None
+    return rec(deepcopy(mat1), deepcopy(mat2), axis, 0)
+
+
+def rec(m1, m2, axis=0, current=0):
+    """recursively calls function"""
+    if axis != current:
+        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
+    m1.extend(m2)
+    return m1
