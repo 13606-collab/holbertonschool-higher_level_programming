bef5cb3e0b64239b0d151483b06260e5aac0300a
13606-collab <shamil1999fcb@gmail.com>
2026-04-30 06:45:19 +0400

    edited: 1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -29,7 +29,7 @@ def determinant(matrix):
     n = len(matrix)
 
     if n == 1:
-        return [[1]]
+        return matrix[0][0]
 
     if n == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
