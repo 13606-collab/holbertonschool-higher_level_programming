313976bc68f8284cd195fb9f0d85648545c3b0e3
13606-collab <shamil1999fcb@gmail.com>
2026-05-09 23:16:29 +0400

    changes to be committed:

--- math/probablity/poisson.py
+++ math/probablity/poisson.py
@@ -1,1 +1,29 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+"""Module for Poisson distribution"""
+
+
+class Poisson:
+    """Represents a Poisson distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes Poisson distribution
+
+        Args:
+            data (list): list of data to estimate the distribution
+            lambtha (float): expected number of occurrences in a given time frame
+
+        Raises:
+            TypeError: if data is not a list
+            ValueError: if data does not contain at least two data points
+            ValueError: if lambtha is not a positive value
+        """
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
