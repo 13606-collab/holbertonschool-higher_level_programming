618593285c4d3daafc357234ee30d68511ab1848
13606-collab <shamil1999fcb@gmail.com>
2026-05-09 23:33:49 +0400

    Add PMF method to Poisson class and improve docstring formatting

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -5,13 +5,14 @@
 class Poisson:
     """Represents a Poisson distribution"""
 
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """Initializes Poisson distribution
 
         Args:
             data (list): list of data to estimate the distribution
-            lambtha (float):
-            expected number of occurrences in a given time frame
+            lambtha (float): expected number of occurrences in a given time frame
 
         Raises:
             TypeError: if data is not a list
@@ -28,3 +29,30 @@ class Poisson:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of successes
+
+        Args:
+            k (int): number of successes
+
+        Returns:
+            float: PMF value for k, or 0 if k is out of range
+        """
+        k = int(k)
+
+        if k < 0:
+            return 0
+
+        # Calculate e^(-λ)
+        e_pow = self.e ** (-self.lambtha)
+
+        # Calculate λ^k
+        lambda_pow = self.lambtha ** k
+
+        # Calculate k! (factorial)
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        return (lambda_pow * e_pow) / factorial
