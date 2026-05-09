2bfd25cbc4d360f139a3600e570e5b6af0a21824
13606-collab <shamil1999fcb@gmail.com>
2026-05-09 23:45:22 +0400

    Implement Exponential distribution class with initialization and validation

---
+++ math/probability/exponential.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Module for Exponential distribution"""
+
+
+class Exponential:
+    """Represents an Exponential distribution"""
+
+    e = 2.7182818285
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes Exponential distribution
+
+        Args:
+            data (list): list of data to estimate the distribution
+            lambtha (float): expected number of occurrences in a given time frame
+
+        Raises:
+            TypeError: if data is not a list
+            ValueError: if data does not contain at least two data points
+            ValueError: if lambtha is not a positive value
+        """
+        if data is not None:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            # For exponential distribution: λ = 1 / mean
+            self.lambtha = float(len(data) / sum(data))
+        else:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
