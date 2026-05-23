53a0775730116c2b723f62c47179d85f259693da
13606-collab <13606@holbertonstudents.com>
2026-05-23 16:26:58 +0000

    more queries 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Lists all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
+
+-- Show grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
