ebee1b4cbe0c6b5b68a869d19bdcaac76a629c30
13606-collab <shamil1999fcb@gmail.com>
2026-05-30 19:45:23 +0400

    fix: refactor line plotting code into a function and improve structure

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,14 +1,18 @@
 ﻿#!/usr/bin/env python3
-
+"""Module for plotting y as a line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 11)
-y = x ** 3
 
-plt.plot(x, y, color='red', linestyle='-')
-plt.xlim(0, 10)
-plt.title('Line Graph')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+def line():
+    """Plot y as a solid red line with x-axis ranging from 0 to 10."""
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    # your code here
+    plt.plot(np.arange(0, 11), y, 'r-')
+    plt.xlim(0, 10)
+    plt.show()
+
+
+line()
