fix: update y calculation to cube of x in 0-line.py

math/plotting/0-line.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.linspace(0, 10)
-y = x
+y = x**3
 
 plt.plot(x, y, "r-")
 plt.xlim(0, 10)