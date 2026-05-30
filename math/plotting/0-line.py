7e2ccd2ef54c54956c835f50244d6d4e3112ebfa
13606-collab <shamil1999fcb@gmail.com>
2026-05-30 19:54:58 +0400

    fix: standardize plot line color and linestyle in line plotting function

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,4 +1,4 @@
-﻿^#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
 """Module for plotting y as a line graph."""
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +8,7 @@ def line():
     """Plot y as a solid red line with x-axis ranging from 0 to 10."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(np.arange(0, 11), y, 'r-')
+    plt.plot(np.arange(0, 11), y, color='red', linestyle='-')
     plt.xlim(0, 10)
     plt.show()
 
