660554aa342fb973f0dd62e21a66ffafb61bd9ab
13606-collab <shamil1999fcb@gmail.com>
2026-05-30 18:11:28 +0400

    feat: enhance line plot with color and axis limits

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,15 @@
-#!/usr/bin/env python3
+﻿#!/usr/bin/env python3
+"""Create a simple line plot of y = x using Matplotlib."""
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 x = np.linspace(0, 10)
 y = x
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Line Plot')
+
+plt.plot(x, y, "r-")
+plt.xlim(0, 10)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Line Plot")
 plt.show()
