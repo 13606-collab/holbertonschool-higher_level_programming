7957f8b6871fa56e5bd6ada1791934b42be76e37
13606-collab <shamil1999fcb@gmail.com>
2026-06-26 21:44:45 +0400

    feat: add sensitivity calculation function for confusion matrix

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Calculates the sensitivity for each class in a confusion matrix."""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """Compute per-class sensitivity from a confusion matrix."""
+    return np.diag(confusion) / np.sum(confusion, axis=1)
