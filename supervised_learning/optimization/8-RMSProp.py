0ded594f7d9fd60b7a6f754d5cd43149c8a13a16
13606-collab <shamil1999fcb@gmail.com>
2026-07-25 22:47:57 +0400

    feat: add create_RMSProp_op function for RMSProp optimization algorithm

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Module that sets up the RMSProp optimization algorithm in
+TensorFlow.
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """Sets up the RMSProp optimization algorithm in TensorFlow.
+
+    Args:
+        alpha (float): the learning rate.
+        beta2 (float): the RMSProp weight (discounting factor).
+        epsilon (float): a small number to avoid division by zero.
+
+    Returns:
+        optimizer: the RMSProp optimizer.
+    """
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha, rho=beta2, epsilon=epsilon)
+    return optimizer
