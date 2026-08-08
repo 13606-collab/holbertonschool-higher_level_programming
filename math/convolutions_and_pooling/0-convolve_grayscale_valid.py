49ba66e6008ae30ffbf046ba6453d2297489a32a
13606-collab <shamil1999fcb@gmail.com>
2026-08-08 19:43:56 +0400

    feat: add convolve_grayscale_valid function for valid convolution on grayscale images

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module that performs a valid convolution on grayscale images."""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """Perform a valid convolution on grayscale images.
+
+    Args:
+        images: numpy.ndarray with shape (m, h, w) containing multiple
+            grayscale images.
+            m is the number of images.
+            h is the height in pixels of the images.
+            w is the width in pixels of the images.
+        kernel: numpy.ndarray with shape (kh, kw) containing the kernel
+            for the convolution.
+            kh is the height of the kernel.
+            kw is the width of the kernel.
+
+    Returns:
+        numpy.ndarray containing the convolved images.
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+    convolved = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            image_slice = images[:, i:i + kh, j:j + kw]
+            convolved[:, i, j] = np.sum(image_slice * kernel, axis=(1, 2))
+
+    return convolved
