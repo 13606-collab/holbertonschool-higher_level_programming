f40b01177102d405f7f6343a77662ee4a99b563e
13606-collab <13606@holbertonschool.com>
2026-08-22 22:47:42 +0400

    add function to change the hue of an image

---
+++ pipeline/data_augmentation/5-hue.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that defines a function to change the hue of an image."""
+import tensorflow as tf
+
+
+def change_hue(image, delta):
+    """Change the hue of an image.
+
+    Args:
+        image: a 3D tf.Tensor containing the image to change.
+        delta: the amount the hue should change.
+
+    Returns:
+        The altered image.
+    """
+    return tf.image.adjust_hue(image, delta)
